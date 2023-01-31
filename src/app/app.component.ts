19a700a1e9897187b96dec0260e92be681ba5d72
Ervin Pepic <pepic_ervin@hotmail.com>
2023-01-31 23:24:56 +0100

    added new place, change logic to use google marker clusterer with filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,108 +6,117 @@ import { PolygonsBoundaries } from './polygons/map-polygons';
 import { StylingMarkers } from './markers/styling/marker-style';
 import { MarkerEvents } from './markers/events/marker-events';
 import { mapStyling } from './map/map-style';
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	@ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
 
-	title = 'Muftijstvo Sandžačko Mape Vakufa';
+  title = 'Muftijstvo Sandžačko Mape Vakufa';
 
-	searchWord?: string = '';
-	allMarkers: Array<any> = [];
+  searchWord?: string = '';
+  allMarkers: Array<any> = [];
+  markerClusters: MarkerClusterer;
 
-	map?: google.maps.Map;
-	mapStyle = mapStyling;
-	mapCenter = new google.maps.LatLng(42.99603931107363, 19.863259815559704);
-	mapZoom = 9
+  map?: google.maps.Map;
+  mapStyle = mapStyling;
+  mapCenter = new google.maps.LatLng(42.99603931107363, 19.863259815559704);
+  mapZoom = 9;
 
-	markerData = MarkerDataSeed;
-	markerEvents = new MarkerEvents();
-	markerStyling = new StylingMarkers();
+  markerData = MarkerDataSeed;
+  markerEvents = new MarkerEvents();
+  markerStyling = new StylingMarkers();
 
-	polygons = new PolygonsBoundaries();
+  polygons = new PolygonsBoundaries();
 
-	mapInitializer(): void {
-		this.map = new google.maps.Map(this.gmap.nativeElement, {
-			center: this.mapCenter,
-			zoom: this.mapZoom,
-			styles: this.mapStyle,
-		});
-		this.addMarkers();
-		this.polygons.drawPolgygons(this.map);
-	}
+  initMap(): void {
+    this.map = new google.maps.Map(this.gmap.nativeElement, {
+      center: this.mapCenter,
+      zoom: this.mapZoom,
+      styles: this.mapStyle,
+    });
+    this.addMarkers();
+    this.polygons.drawPolgygons(this.map);
+  }
 
-	ngAfterViewInit(): void {
-		this.mapInitializer();
-	}
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 
-	addMarkers() {
-		return this.markerData.map(extractedMarkerData => {
-			const marker = new google.maps.Marker({
-				...extractedMarkerData,
-				position: new google.maps.LatLng(extractedMarkerData.position),
-				icon: this.markerStyling.markerIconDefaultCreate(),
-				draggable: false,
-				optimized: false,
-				animation: google.maps.Animation.DROP,
-			});
-			marker.setMap(this.map);
-			this.allMarkers.push(marker);
+  addMarkers() {
+    this.markerData.map((extractedMarkerData) => {
+      const marker = new google.maps.Marker({
+        ...extractedMarkerData,
+        position: new google.maps.LatLng(extractedMarkerData.position),
+        icon: this.markerStyling.markerIconDefaultCreate(),
+        draggable: false,
+        optimized: false,
+        animation: google.maps.Animation.DROP,
+      });
 
-			this.markerEvents.markerInfoWindow(marker, extractedMarkerData, this.map);
-			this.markerEvents.markerMouseOver(marker);
-			this.markerEvents.markerMouseOut(marker);
-		});
-	}
+      this.markerEvents.markerInfoWindow(marker, extractedMarkerData, this.map);
+      this.markerEvents.markerMouseOver(marker);
+      this.markerEvents.markerMouseOut(marker);
 
-	searchMarkersFilter(event: any): any {
-		let bounds = new google.maps.LatLngBounds();
-		for (let marker of this.allMarkers) {
-			if (this.searchWord === '' || event === '') {
-				marker.setVisible(true);
-				this.map.setZoom(this.mapZoom);
-				this.map.setCenter(this.mapCenter);
-			}
-		}
-		if (this.allMarkers && this.searchWord) {
-			return this.allMarkers.filter((marker) => {
-				if (this.searchWord === '' || event === '') {
-					marker.setVisible(true);
-					this.map.setZoom(this.mapZoom);
-					this.map.setCenter(this.mapCenter);
-				}
-				else if (
-					marker.placeName.toLowerCase().indexOf(event.toLowerCase()) > -1 ||
-					marker.cadastarMunicipality.toLowerCase().indexOf(event.toLowerCase()) > -1
-				) {
-					marker.setVisible(true);
-					bounds.extend(marker.getPosition());
-				}
-				else {
-					marker.setVisible(false);
-				}
-				this.map.fitBounds(bounds);
-			});
-		}
-		return this.allMarkers;
-	}
+      marker.setMap(this.map);
+      this.allMarkers.push(marker);
 
-	selectMarkerFilter(event: any) {
-		let bounds = new google.maps.LatLngBounds();
-		for (let marker of this.allMarkers) {
-			if (marker.placeFilter == event.target.value || event.target.value === "") {
-				marker.setVisible(true);
-				bounds.extend(marker.getPosition());
-			}
-			else {
-				marker.setVisible(false);
-			}
-			this.map.fitBounds(bounds);
-		}
-	}
+      return marker;
+    });
+    this.markerClusters = new MarkerClusterer({
+      map: this.map,
+      markers: this.allMarkers,
+    });
+  }
 
+  searchMarkersFilter(event: any): any {
+    let bounds = new google.maps.LatLngBounds();
+    for (let marker of this.allMarkers) {
+      if (this.searchWord === '' || event === '') {
+        marker.setVisible(true);
+        this.map.setZoom(this.mapZoom);
+        this.map.setCenter(this.mapCenter);
+      }
+    }
+    if (this.allMarkers && this.searchWord) {
+      return this.allMarkers.filter((marker) => {
+        if (this.searchWord === '' || event === '') {
+          marker.setVisible(true);
+          this.map.setZoom(this.mapZoom);
+          this.map.setCenter(this.mapCenter);
+        } else if (
+          marker.placeName.toLowerCase().indexOf(event.toLowerCase()) > -1 ||
+          marker.cadastralParcelNumber
+            .toLowerCase()
+            .indexOf(event.toLowerCase()) > -1 ||
+          marker.cadastarMunicipality
+            .toLowerCase()
+            .indexOf(event.toLowerCase()) > -1
+        ) {
+          marker.setVisible(true);
+          bounds.extend(marker.getPosition());
+        } else {
+          marker.setVisible(false);
+        }
+        this.map.fitBounds(bounds);
+      });
+    }
+    return this.allMarkers;
+  }
+
+  selectMarkerFilter(event: any) {
+    let bounds = new google.maps.LatLngBounds();
+    this.markerClusters.clearMarkers();
+    const filteredMarkers = this.allMarkers.filter(
+      (marker) =>
+        marker.placeFilter == event.target.value &&
+        bounds.extend(marker.getPosition())
+    );
+    this.markerClusters.addMarkers(filteredMarkers);
+    this.map.fitBounds(bounds);
+  }
 }
